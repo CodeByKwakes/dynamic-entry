2bf10af7efcc0dfecd20aa74c62cb22ac22831ec
Kwakes Prempeh <kwakesproject@gmail.com>
2018-07-23 07:01:12 +0100

    moved from component to directive

--- src/app/message/message-entry.directive.ts
+++ src/app/message/message-entry.directive.ts
@@ -1,10 +1,41 @@
-import { Directive, ViewContainerRef } from '@angular/core';
+import {
+  ComponentFactoryResolver, ComponentRef, Directive,
+  OnChanges, OnInit, ViewContainerRef, OnDestroy
+} from '@angular/core';
+import { MessageTemplateBase } from './message-template-base';
+import { MessageComponent } from './message/message.component';
 
 @Directive({
   selector: '[appMessageEntry]'
 })
-export class MessageEntryDirective {
+export class MessageEntryDirective
+  extends MessageTemplateBase
+  implements OnChanges, OnInit, OnDestroy {
 
-  constructor(public viewContainer: ViewContainerRef) { }
+  componentRef: ComponentRef<MessageTemplateBase>;
 
+  constructor(
+    public viewContainer: ViewContainerRef,
+    private resolver: ComponentFactoryResolver) {
+    super();
+  }
+
+  ngOnChanges() {
+    if (this.componentRef) {
+      this.componentRef.instance.message = this.message;
+      this.componentRef.instance.clicked = this.clicked;
+    }
+  }
+
+  ngOnInit(): void {
+    this.viewContainer.clear();
+    const factory = this.resolver.resolveComponentFactory<MessageTemplateBase>(MessageComponent);
+    this.componentRef = this.viewContainer.createComponent(factory);
+    this.componentRef.instance.message = this.message;
+    this.componentRef.instance.clicked = this.clicked;
+  }
+
+  ngOnDestroy(): void {
+    this.componentRef.destroy();
+  }
 }
--- src/app/message/message-item/message-item.component.ts
+++ src/app/message/message-item/message-item.component.ts
@@ -1,44 +1,9 @@
-import { MessageEntryDirective } from './../message-entry.directive';
-import {
-  Component, OnInit, Input,
-  Output, EventEmitter, ViewChild, ViewContainerRef, ComponentRef, ComponentFactoryResolver, OnChanges
-} from '@angular/core';
+import { Component } from '@angular/core';
 import { MessageTemplateBase } from '../message-template-base';
-import { MessageComponent } from '../message/message.component';
 
 @Component({
   selector: 'app-message-item',
   templateUrl: './message-item.component.html',
   styleUrls: ['./message-item.component.css']
 })
-export class MessageItemComponent implements OnInit, OnChanges {
-  @Input() message: string;
-  @Output() clicked: EventEmitter<string> = new EventEmitter<string>();
-  @ViewChild(MessageEntryDirective) entry: MessageEntryDirective;
-  componentRef: ComponentRef<MessageTemplateBase>;
-
-  constructor(private resolver: ComponentFactoryResolver) { }
-
-  ngOnChanges() {
-    if (this.componentRef) {
-      this.componentRef.instance.message = this.message;
-      this.componentRef.instance.clicked = this.clicked;
-    }
-  }
-
-  ngOnInit() {
-    this.createComponent();
-  }
-
-  createComponent() {
-    this.entry.viewContainer.clear();
-    const factory = this.resolver.resolveComponentFactory<MessageTemplateBase>(MessageComponent);
-    this.componentRef = this.entry.viewContainer.createComponent(factory);
-    this.componentRef.instance.message = this.message;
-    this.componentRef.instance.clicked = this.clicked;
-  }
-
-  destroyComponent() {
-    this.componentRef.destroy();
-  }
-}
+export class MessageItemComponent extends MessageTemplateBase { }
